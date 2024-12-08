c07fd775d906553fff22695d3ec042b035c5ea8e
vishal <sloppy_emotion@vishals-MacBook-Air.local>
2024-12-08 17:35:16 +0530

    userLoginPost is implemented

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -205,7 +205,51 @@ func (app *application) userLogin(w http.ResponseWriter, r *http.Request){
 }
 
 func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "Authenticate and login the user...")
+	var form userLoginForm
+
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, r, http.StatusBadRequest)
+		return
+	}
+
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+	
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "login.tmpl.html", data)
+		return
+	}
+
+	id, err := app.users.Authenticate(form.Email, form.Password)
+	if err != nil {
+		if errors.Is(err, models.ErrInvalidCredentials) {
+			form.AddNonFieldError("Email or Password is incorrect")
+			data := app.newTemplateData(r)
+			data.Form = form
+			app.render(w, r, http.StatusUnprocessableEntity, "login.tmpl.html", data)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
+
+	// Use the RenewToken method on the current session to change the session ID. It's good practice to 
+	// generate a new seesion ID when the authentication state or privilege levels changes for the user
+	// (e.g. login and logout operation)	
+	err = app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	// Add the ID of the current user to the session, so that they are now logged in
+	app.sessionManager.Put(r.Context(), "authenticatedUserID", id)
+
+	http.Redirect(w, r, "/snippet/create/", http.StatusSeeOther)
+
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request){
--- internal/models/users.go
+++ internal/models/users.go
@@ -51,7 +51,32 @@ func (m *UserModel) Insert(name, email, password string) error {
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	// Retrieve the id and hashed password for the given email. If no matching email exists we return
+	// the ErrInvalidCredentials error.
+	var id int
+	var hashedPassword []byte
+	
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
+
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} 
+		return 0, err
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword){
+			return 0, ErrInvalidCredentials
+		}
+
+		return 0, err
+	}
+
+	return id, nil
+
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
