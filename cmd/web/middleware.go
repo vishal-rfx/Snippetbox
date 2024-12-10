dd6ae62d80700d2a1f7f6c02c4dffb4d9e4eae5a
vishal <sloppy_emotion@vishals-MacBook-Air.local>
2024-12-10 09:44:04 +0530

    Require authentication middleware is added and configured to use in /CREATE and /LOGOUT handlers

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -46,3 +46,20 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request){
+		// If the user is not authenticated, redirect them to the login page and return from middleware chain so
+		// that no subsequent handlers in the chan are executed
+		if !app.isAuthenticated(r){
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+
+		// Otherwise set the Cache-Control: no-store header so that pages require 
+		// authentication are not stored in the users browser cache ( or other intermediary cache)
+		w.Header().Add("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+
+	})
+}
